16941c82a30f56fcb9a5329f938d62f8ff393df0
Viktoriia Romanova <khoviktoriya@yandex.ru>
2024-04-26 23:05:27 +0700

    Adds hyperparameter loading tests

---
+++ tests/utils/test_hyperparameter_loading.py
@@ -0,0 +1,24 @@
+import unittest
+import yaml
+from dataclasses import asdict
+
+from animator.utils.parameter_storages import TrainingParams
+
+def dict_factory(args) -> dict:
+    res = {}
+    for key, val in args:
+        if isinstance(val, tuple):
+            res[key] = list(val)
+        else:
+            res[key] = val
+    return res
+
+class ParameterLoadingTests(unittest.TestCase):
+    def test_load_train_param_correct(self):
+        path = 'animator/train_eval/style_transfer/hyperparameters.yaml'
+        with open(path, 'r') as file:
+            params = yaml.safe_load(file)
+        
+        loaded_params = TrainingParams(**params)
+
+        self.assertTrue(params == asdict(loaded_params, dict_factory = dict_factory))
