4f906bda9a1b628130d1c64eb4be7403d6c435c6
Viktoriia Romanova <khoviktoriya@yandex.ru>
2024-07-05 19:13:53 +0700

    Adds training process and logs collection for local(not DataSphere) pipeline - the process starts on the remote machine in a Docker container using Docker context - results will be copied back to the initial machine

--- train_eval/local/start_logger.sh
+++ train_eval/local/start_logger.sh
@@ -6,4 +6,6 @@ set -e
 cd $(dirname "$0")
 
 nohup docker --context remote-machine logs -f animator 1>tmp/stdout.txt 2>/dev/null &
+echo "Automatic logs collection was started"
+echo "Run VScode tensorboard to visualize them (Ctrl + Shift + P -> Python: Launch TensorBoard)"
 python3 -m animator.utils.logs.logger --path tmp/stdout.txt
