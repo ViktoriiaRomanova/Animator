456b4e79d65bc0900b387603f9e0c22a6e567e1e
Viktoriia Romanova <khoviktoriya@yandex.ru>
2024-12-27 18:05:52 +0700

    continue construction of autoencoder

--- animator/diffusion/autoencoder.py
+++ animator/diffusion/autoencoder.py
@@ -1,7 +1,9 @@
 from diffusers import AutoencoderKL
 from torch import nn, Generator, Tensor
-from diffusers.models.autoencoders.vae import DecoderOutput
+from diffusers.models.autoencoders.vae import DecoderOutput, DiagonalGaussianDistribution
+from diffusers.models.modeling_outputs import AutoencoderKLOutput
 from huggingface_hub import PyTorchModelHubMixin
+from warnings import warn
 
 
 def encoder_forward(self, x: Tensor) -> Tensor:
@@ -21,9 +23,14 @@ def decoder_forward(self, x: Tensor, latent_embeds: Tensor | None = None) -> Ten
     x = self.conv_in(x)
     x = self.mid_block(x)
     len_skip = len(self.incoming_skip) - 1
-    for ind, up_block in enumerate(self.up_blocks):
-        up_skip = self.skip[ind](self.incoming_skip[len_skip - ind] * self.gamma)
-        x = up_block(x + up_skip, latent_embeds)
+    if len_skip == -1:
+        warn("Missing skip connection data")
+        for ind, up_block in enumerate(self.up_blocks):
+            x = up_block(x, latent_embeds)
+    else:
+        for ind, up_block in enumerate(self.up_blocks):
+            up_skip = self.skip[ind](self.incoming_skip[len_skip - ind] * self.gamma)
+            x = up_block(x + up_skip, latent_embeds)
     if latent_embeds is None:
         x = self.conv_norm_out(x)
     else:
@@ -37,19 +44,13 @@ class SCAutoencoderKL(nn.Module, PyTorchModelHubMixin):
 
     def __init__(self, gamma: float = 1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.vae = AutoencoderKL.from_pretrained(
-            "stabilityai/sd-turbo", subfolder="vae"
-        )
-        self.vae.encoder.forward = encoder_forward.__get__(
-            self.vae.encoder, self.vae.encoder.__class__
-        )
-        self.vae.decoder.forward = decoder_forward.__get__(
-            self.vae.decoder, self.vae.decoder.__class__
-        )
+        self.vae = AutoencoderKL.from_pretrained("stabilityai/sd-turbo", subfolder="vae")
+        self.vae.encoder.forward = encoder_forward.__get__(self.vae.encoder, self.vae.encoder.__class__)
+        self.vae.decoder.forward = decoder_forward.__get__(self.vae.decoder, self.vae.decoder.__class__)
         self.vae.encoder.down_skip = []
         self.vae.decoder.incoming_skip = []
         self.vae.decoder.gamma = gamma
-        self.skip = nn.ModuleList(
+        self.vae.decoder.skip = nn.ModuleList(
             [
                 nn.Conv2d(512, 512, 1, 1, bias=False),
                 nn.Conv2d(256, 512, 1, 1, bias=False),
@@ -57,6 +58,12 @@ class SCAutoencoderKL(nn.Module, PyTorchModelHubMixin):
                 nn.Conv2d(128, 256, 1, 1, bias=False),
             ]
         )
+
+    def decode(
+        self,
+        x: Tensor, *args, **kwargs
+    ) -> AutoencoderKLOutput | tuple[DiagonalGaussianDistribution]:
+        return self.vae.decode(x, *args, **kwargs)
 
     def forward(
         self,
