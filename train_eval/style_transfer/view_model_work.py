c7f382a4d6752d7720d0726ffb43d823d082ba68
Viktoriia Romanova <khoviktoriya@yandex.ru>
2025-01-09 21:48:42 +0700

    Minor adjustments for usage.

--- train_eval/style_transfer/view_model_work.py
+++ train_eval/style_transfer/view_model_work.py
@@ -13,16 +13,16 @@ HYPERPARAMETERS = 'train_eval/style_transfer/hyperparameters.yaml'
 MODEL1_WEIGHTS = 'train_eval/local/train_checkpoints/199_old.pt'
 MODEL2_WEIGHTS = 'train_eval/style_transfer/train_checkpoints/ukiyoe/199.pt'
 #IMG_PATH = 'datasets/transform/domainX'
-IMG_PATH = '/home/viktoriia/Pictures/tmp/'
-#IMG_PATH = '/home/viktoriia/Downloads/transfer_test/domainX'
+#IMG_PATH = '/home/viktoriia/Pictures/tmp/'
+IMG_PATH = '/home/viktoriia/Downloads/transfer_test/domainY'
 
 
 if __name__ == '__main__':
     with open(HYPERPARAMETERS, 'r') as file:
         data_transform = TrainingParams(**yaml.safe_load(file)).data
-    batch_size = 1 # for now keep it this way, 
-    # TODO: dataloader can't combine images of different size
-    names = get_data(IMG_PATH)
+    batch_size = 1 # keep batch_size == 1 for different size images 
+    # dataloader can't combine images of different size
+    names = get_data(IMG_PATH)[20:25]
     imges = PostProcessingDataset(IMG_PATH, names,
                                   data_transform.size,
                                   data_transform.mean,
@@ -36,10 +36,10 @@ if __name__ == '__main__':
         img = img * tensor(data_transform.std) + tensor(data_transform.mean)
         return img
     
-    model_based_img_processor1 = ModelImgProcessing(Generator(), 'genA', MODEL1_WEIGHTS,
+    model_based_img_processor1 = ModelImgProcessing(Generator(), 'genB', MODEL1_WEIGHTS,
                                                    mode = 'simple',
                                                    transform = img_transformation)
-    model_based_img_processor2 = ModelImgProcessing(Generator(), 'genA', MODEL2_WEIGHTS,
+    model_based_img_processor2 = ModelImgProcessing(Generator(), 'genB', MODEL2_WEIGHTS,
                                                    mode = 'simple',
                                                    transform = img_transformation)
     img_processor = ImgProcessing(img_transformation)
