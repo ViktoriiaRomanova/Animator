e29b0cb35ff76d47c58ba74c4f1585f6cca07ab4
Viktoriia Romanova <khoviktoriya@yandex.ru>
2025-01-09 23:52:45 +0700

    fix flake8 warnings

--- animator/diffusion/autoencoder.py
+++ animator/diffusion/autoencoder.py
@@ -1,12 +1,14 @@
+from warnings import warn
+
 from diffusers import AutoencoderKL
-from torch import nn, Generator, Tensor
 from diffusers.models.autoencoders.vae import DecoderOutput, DiagonalGaussianDistribution
 from diffusers.models.modeling_outputs import AutoencoderKLOutput
-from warnings import warn
-from peft import LoraConfig, get_peft_model
+from peft import get_peft_model, LoraConfig
+from torch import Generator, nn, Tensor
 
 
 def encoder_forward(self, x: Tensor) -> Tensor:
+    """Forward method for encoder(AutoencoderKL) with skip connections functionality."""
     self.down_skip = []
     x = self.conv_in(x)
     for down_block in self.down_blocks:
@@ -20,12 +22,13 @@ def encoder_forward(self, x: Tensor) -> Tensor:
 
 
 def decoder_forward(self, x: Tensor, latent_embeds: Tensor | None = None) -> Tensor:
+    """Forward method for decoder(AutoencoderKL) with skip connections functionality."""
     x = self.conv_in(x)
     x = self.mid_block(x)
     len_skip = len(self.incoming_skip) - 1
     if len_skip == -1:
-        warn("Missing skip connection data")
-        for ind, up_block in enumerate(self.up_blocks):
+        warn('Missing skip connection data')
+        for up_block in self.up_blocks:
             x = up_block(x, latent_embeds)
     else:
         for ind, up_block in enumerate(self.up_blocks):
@@ -41,10 +44,25 @@ def decoder_forward(self, x: Tensor, latent_embeds: Tensor | None = None) -> Ten
 
 
 class SCAutoencoderKL(nn.Module):
+    """Customize pretrained AutoencoderKL."""
 
-    def __init__(self, rank: int = 4, gamma: float = 1, *args, **kwargs):
+    def __init__(self, rank: int = 4, gamma: float = 1, *args, **kwargs) -> None:
+        """
+            Create castom AutoencoderKL.
+
+            - load pretrained AutoencoderKL
+            - change forward mathods
+            - add skip connections
+            - initialize skip connections weights
+            - add LoRA adapters
+
+            Parameters:
+                rank - the inner dimension of the low-rank matrices
+                gamma - grade of influence of skip connections
+
+        """
         super().__init__(*args, **kwargs)
-        self.vae = AutoencoderKL.from_pretrained("stabilityai/sd-turbo", subfolder="vae")
+        self.vae = AutoencoderKL.from_pretrained('stabilityai/sd-turbo', subfolder='vae')
         self.vae.encoder.forward = encoder_forward.__get__(self.vae.encoder, self.vae.encoder.__class__)
         self.vae.decoder.forward = decoder_forward.__get__(self.vae.decoder, self.vae.decoder.__class__)
         self.vae.encoder.down_skip = []
@@ -68,28 +86,29 @@ class SCAutoencoderKL(nn.Module):
 
         # Target modules names for Lora
         encoder_param_names = [
-            "conv1",
-            "conv2",
-            "conv_in",
-            "conv_shortcut",
-            "conv",
-            "conv_out",
-            "to_k",
-            "to_q",
-            "to_v",
-            "to_out.0",
+            'conv1',
+            'conv2',
+            'conv_in',
+            'conv_shortcut',
+            'conv',
+            'conv_out',
+            'to_k',
+            'to_q',
+            'to_v',
+            'to_out.0',
         ]
-        module_names_to_keep = ["decoder.skip.0", "decoder.skip.1", "decoder.skip.2", "decoder.skip.3"]
+        module_names_to_keep = ['decoder.skip.0', 'decoder.skip.1', 'decoder.skip.2', 'decoder.skip.3']
 
         lora_config = LoraConfig(
             r=rank,
-            init_lora_weights="gaussian",
+            init_lora_weights='gaussian',
             target_modules=encoder_param_names,
             modules_to_save=module_names_to_keep,
         )
         self.vae = get_peft_model(self.vae, lora_config)
 
     def decode(self, x: Tensor, *args, **kwargs) -> AutoencoderKLOutput | tuple[DiagonalGaussianDistribution]:
+        """Decode rescale and sample images."""
         return self.vae.decode(x, *args, **kwargs)
 
     def forward(
@@ -99,7 +118,8 @@ class SCAutoencoderKL(nn.Module):
         return_dict: bool = True,
         generator: Generator | None = None,
     ) -> DecoderOutput | Tensor:
-
+        """Forward pass of autoencoder with additional net."""
+        # TODO add UNet into chain
         posterior = self.encode(x).latent_dist
         self.decoder.incoming_skip = self.encoder.down_skip
         if sample_posterior:
